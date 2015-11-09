41a445522e6e5fd59a6b971e19f3eb1eb5893da2
Noah Tilton <noah@tilton.co>
2015-11-09 11:16:17 -0600

    observe games working

--- bobby/events.py
+++ bobby/events.py
@@ -1,12 +1,12 @@
-from threading import Thread
+from threading import Thread, Lock
 
 from flask.ext.socketio import emit as bare_emit
 
 from bobby import sio, app, curry_emit
-from remote import fics
-from logger import clog, slog, log
+from remote import fics_r, fics_w
+from logger import slog # , clog, log
 
-connection  = None
+sock_lock   = None
 emit        = curry_emit(bare_emit)
 
 # each one of the following corresponds to client event
@@ -15,11 +15,14 @@ emit        = curry_emit(bare_emit)
 def handle_connected():
     slog('Client connected')
     emit('Websocket connection established.\n')
-    global connection
-    if connection is None:
-        connection = Thread(target=fics)
-        connection.start()
-        slog('started thread')
+    global sock_lock
+    if sock_lock is None:
+        sock_lock = Lock()
+        reader = Thread(target=fics_r, args=(sock_lock,))
+        reader.start()
+        writer = Thread(target=fics_w, args=(sock_lock,))
+        writer.start()
+        slog('started reader')
 
 @sio.on('command')
 def handle_command(command):
--- bobby/remote.py
+++ bobby/remote.py
@@ -1,96 +1,113 @@
-from traceback import format_exc
 import telnetlib
+from re import sub, split
+from time import sleep
+from traceback import format_exc
 from Queue import Empty
 
 from bobby import app, sio, curry_emit
 from config import FICS_HOST, FICS_PORT
-from logger import clog, slog, log
+from logger import clog # , slog, log
 
-first_connect = True
+#
+EMIT                    = curry_emit(sio.emit)
+TN                      = None
+CONFIGURED              = False
+TIMEOUT                 = 3
+PRELIMINARY_COMMANDS    = """\
+set seek 0
+set bell off
+set style 12
+iset nowrap 1
+iset ms 1
+iset gameinfo 1
+-channel 53
+set provshow 1
+set interface 'BOBBY (http://github.com/noah/bobby)'\
+""".split('\n')
 
-PRELIMINARY_COMMANDS = [
-    'set seek 0',
-    'set style 12',
-]
+def is_style12(s):
+    return s[:4] == '<12>'
 
-def fics():
-    def style12(s):
-        print "`{}'".format(s[:4])
-        return s[:4] == '<12>'
+def parse_style12(s):
+    keys = "s12 row1 row2 row3 row4 row5 row6 row7 row8 turn_color\
+        double_push_file white_castle_short white_castle_long\
+        black_castle_short black_castle_long moves_since_irreversible\
+        game_number white_name black_name my_relation initial_time_secs\
+        increment_time_secs white_strength black_strength white_time_remain\
+        black_time_remain next_move_number coordinate_notation\
+        previous_move_time pretty_notation_previous flip_field"
+    return dict(zip(keys.split(), s.split(' ')))
 
-    def parse12(s):
-        keys = """\
-        s12
-        row1
-        row2
-        row3
-        row4
-        row5
-        row6
-        row7
-        row8
-        turn_color
-        double_push_file
-        white_castle_short
-        white_castle_long
-        black_castle_short
-        black_castle_long
-        moves_since_irreversible
-        game_number
-        white_name
-        black_name
-        my_relation
-        initial_time_secs
-        increment_time_secs
-        white_strength
-        black_strength
-        white_time_remain
-        black_time_remain
-        next_move_number
-        coordinate_notation
-        previous_move_time
-        pretty_notation_previous
-        flip_field
-        """
-        return zip(keys.split(), s.split(' '))
-
-
+def fen_from_style12(s):
     #
-    global first_connect
-    TIMEOUT = 3
-    emit    = curry_emit(sio.emit)
-    emit('Opening connection to freechess.org\n')
-    try:
-        slog('Opening telnet')
-        tn = telnetlib.Telnet(FICS_HOST, FICS_PORT)
+    fen = []
+    for i in xrange(1, 9):
+        fen.append(s['row{}'.format(i)])
+    fen = '/'.join(fen)
+    #
+    foo = '9' # <- fen format known not to contain this char
+    fen = fen.replace('-', foo)
+    fen_split = split(r'\D', fen)
+    for fs in fen_split:
+        l = len(fs)
+        if l > 0:
+            fen = sub('9+', str(l), fen, count=1)
+    return fen
 
-        emit(tn.read_until('login:', TIMEOUT))
-        # try login as guest
-        # https://docs.python.org/2/library/telnetlib.html#telnetlib.Telnet.expect
-        tn.write('guest\n')
-        match_index, match, text = tn.expect(['Press return to enter the server as.*$'], TIMEOUT)
-        if match_index == 0: # matched
-            emit(text)
-            tn.write('\n\n')
-            while True:
-                # send telnet output to web client
-                output = tn.read_until('fics%', TIMEOUT)
-                style12(output)
-                #    board = parse12(output)
-                #    print board
-                #    emit(output)
-                # send preliminary (configuration) packets
-                if first_connect:
-                    [emit(tn.write("{}\n".format(cmd))) for cmd in PRELIMINARY_COMMANDS]
-                    first_connect = False
-                # send web commands to telnet
-                try:
-                    command = app.command_queue.get_nowait() # don't block
-                    clog("{}".format(command))
-                    emit(tn.write("{}\n".format(command)))
-                except Empty:
-                    pass
+def fics_r(lock):
+    global TN, CONFIGURED
+
+    assert(TN is None)
+
+    EMIT('Opening telnet connection to {}:{}\n'.format(FICS_HOST, FICS_PORT))
+    try:
+        # login
+        TN = telnetlib.Telnet(FICS_HOST, FICS_PORT)
+        EMIT(TN.read_until('login:', TIMEOUT))
+        TN.write('guest\n')
+        match_index, match, text = TN.expect(['Press return to enter the server as.*$'], TIMEOUT)
+        assert (match_index == 0)
+        EMIT(text); TN.write('\n\n'); sio.emit('ficsup', {'data': True})
+        [app.command_queue.put(cmd) for cmd in PRELIMINARY_COMMANDS]
+        CONFIGURED = True; print 'reader configured.'
+
+        while 1:
+            try:
+                if lock.acquire(blocking=False):
+                    output = TN.read_until('fics% ').strip()
+                    if len(output):
+                        lines = output.split('\n\r')
+                        lines = lines[:-1] # prompt is always last, so omit it
+                        if len(lines):
+                            if is_style12(lines[0]):
+                                print lines[0]
+                                s12 = parse_style12(lines[0])
+                                orientation = 'white'
+                                if s12['flip_field'] == 1: orientation = 'black'
+                                sio.emit('fen', {'data' :
+                                                 fen_from_style12(s12),
+                                                 'orientation' : 'white' or orientation
+                                                 } )
+                            else:
+                                [EMIT('{}\n'.format(line)) for line in lines]
+            finally:
+                lock.release()
     except:
-        emit(format_exc())
-        tn.close()
-        return False
+        EMIT(format_exc())
+        TN.close()
+
+def fics_w(lock):
+        global TN, CONFIGURED
+        while not CONFIGURED:
+            sleep(1)
+        while 1:
+            # If we have any data, send it:
+            try:
+                if lock.acquire(0):
+                    command = app.command_queue.get()
+                    clog("got cmd: {}".format(command))
+                    EMIT(TN.write("{}\n".format(command)))
+            except Empty:
+                pass
+            finally:
+                lock.release()
